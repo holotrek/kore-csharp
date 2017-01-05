78f14ecda63355fc42f08eff4c14625f37bd1e0a
holotrek <evshell18@gmail.com>
2017-01-05 10:40:55 -0500

    Updated package release notes.

--- Kore/Nuget/push_packages.bat
+++ Kore/Nuget/push_packages.bat
@@ -1,13 +1,12 @@
-nuget push Kore.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Domain.EF.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Domain.RavenDb.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Providers.Caching.Memory.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Providers.Containers.TinyIoC.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Providers.Containers.Unity.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Providers.Logging.Log4Net.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Providers.Logging.NLog.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Providers.Messages.Resource.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Providers.Pdf.ITextSharp.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Providers.Reporting.SSRS.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push Kore.Providers.Serialization.Newtonsoft.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push YourPackage.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push Kore.1.1.1.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push Kore.Domain.EF.1.1.1.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push Kore.Domain.RavenDb.1.1.1.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push Kore.Providers.Caching.Memory.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push Kore.Providers.Containers.TinyIoC.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push Kore.Providers.Containers.Unity.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push Kore.Providers.Logging.Log4Net.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push Kore.Providers.Logging.NLog.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push Kore.Providers.Messages.Resource.1.1.2.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push Kore.Providers.Pdf.ITextSharp.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push Kore.Providers.Reporting.SSRS.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push Kore.Providers.Serialization.Newtonsoft.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
